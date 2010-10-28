DuendeInexistente/libora

commit c1e55691fbdd3319d1d205281991de78588468b3

Group layer attributes into a struct
Use this for ora_read_layer()

src/lib/ora.c
@@ -601,7 +601,7 @@ extern int ora_stack_type(ORA ora)
 
 }
 
-extern int ora_read_layer(ORA ora, ubyte** data, ora_rectangle* geometry, int* format, float* opacity, ora_progress_callback callback)
+extern int ora_read_layer(ORA ora, ora_layer* layer, ora_progress_callback callback)
 {
     unz_file_info file_info;
     ora_document_read* read_struct;
@@ -657,20 +657,11 @@ extern int ora_read_layer(ORA ora, ubyte** data, ora_rectangle* geometry, int* f
         return ORA_ERROR;
     }
 
-    if (ora_read_raster((ora_document*)ora, zip, data, &(geometry->width), &(geometry->height), format, callback) < 0)
+    if (ora_read_raster((ora_document*)ora, zip, layer, callback) < 0)
     {
         ORA_SET_ERROR(ora, ORA_ERROR_READ);
         return ORA_ERROR;
     }
-
-    if (geometry) 
-    {
-        geometry->x = layer_data->bounds.x;
-        geometry->y = layer_data->bounds.y;
-    }
-
-    if (opacity)
-        *opacity = layer_data->opacity;
 
     if (unzCloseCurrentFile (zip) != UNZ_OK)
     {

src/lib/layers/layers.c
@@ -76,7 +76,7 @@ void _ora_png_flush_data(png_structp png_ptr)
     //TODO: flushing
 }
 
-int ora_read_raster(ora_document* document, unzFile zip, ubyte** data, int* width, int* height, int* format, ora_progress_callback callback)
+int ora_read_raster(ora_document* document, unzFile zip, ora_layer* layer, ora_progress_callback callback)
 {
     png_structp png_ptr;
     png_infop info_ptr;
@@ -86,8 +86,10 @@ int ora_read_raster(ora_document* document, unzFile zip, ubyte** data, int* widt
     long image_size;
     int line_size;
     ubyte* image_data;
+    int width, height;
     int current_line;
     int current_progress = 0, p;
+
 /*
     FILE *fp = fopen(file_name, "rb");
     if (!fp)
@@ -154,8 +156,8 @@ png_set_sig_bytes(png_ptr, number);
     }
 
 
-    *width = png_get_image_width(png_ptr, info_ptr);
-    *height = png_get_image_height(png_ptr, info_ptr);
+    width = png_get_image_width(png_ptr, info_ptr);
+    height = png_get_image_height(png_ptr, info_ptr);
     bit_depth = png_get_bit_depth(png_ptr, info_ptr);
     color_type = png_get_color_type(png_ptr,info_ptr);
 
@@ -168,18 +170,18 @@ png_set_sig_bytes(png_ptr, number);
     if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
         png_set_tRNS_to_alpha(png_ptr);
 
-    line_size = (*width) * (bit_depth < 16 ? 1 : 2) * (color_type == PNG_COLOR_TYPE_RGBA ? 4 : 3);
-    image_size = line_size * (*height);
+    line_size = width * (bit_depth < 16 ? 1 : 2) * (color_type == PNG_COLOR_TYPE_RGBA ? 4 : 3);
+    image_size = line_size * height;
 
     image_data = (ubyte*) malloc(sizeof(ubyte) * image_size);
 
-    for (current_line = 0; current_line < *height; current_line++)
+    for (current_line = 0; current_line < height; current_line++)
     {
         png_read_row(png_ptr, image_data + line_size * current_line, NULL);
 
         if (callback)
         {
-            p = (current_line * 100) / (*height - 1);
+            p = (current_line * 100) / (height - 1);
             if (current_progress != p) 
             {
                 callback(p);
@@ -206,8 +208,11 @@ png_set_sig_bytes(png_ptr, number);
         return ORA_ERROR;
     }
 
-    *data = image_data;
-    *format = ORA_FORMAT_RASTER | (color_type == PNG_COLOR_TYPE_RGBA ? ORA_FORMAT_ALPHA : 0) | (bit_depth < 16 ? 0 : ORA_FORMAT_DOUBLE);
+    /* Set ouput variables */
+    layer->geometry.width = width;
+    layer->geometry.height = height;
+    layer->data = image_data;
+    layer->format = ORA_FORMAT_RASTER | (color_type == PNG_COLOR_TYPE_RGBA ? ORA_FORMAT_ALPHA : 0) | (bit_depth < 16 ? 0 : ORA_FORMAT_DOUBLE);
 
     return ORA_OK;
 }

src/lib/layers/layers.h
@@ -40,14 +40,11 @@
  *
  * @param document  the document
  * @param zip 	    the ZIP archive
- * @param data 	    the output parameter that will contain the data of the raster
- * @param width 	the output parameter that will contain the width of the raster
- * @param height 	the output parameter that will contain the height of the raster
- * @param format 	the output parameter that will contain the format of the raster
+ * @param layer     the layer to read data into
  * @param callback 	an optional callback function that can be used to retrieve progress information of the operation. 
  *
  */
-int ora_read_raster(ora_document* document, unzFile zip, ubyte** data, int* width, int* height, int* format, ora_progress_callback callback);
+int ora_read_raster(ora_document* document, unzFile zip, ora_layer* layer, ora_progress_callback callback);
 
 /**
  * @brief Internal function that handles writing the raster layer to the file.

src/lib/ora.h
@@ -175,6 +175,14 @@ typedef struct _ora_rectangle
     int height; /**< height height of the layer */
 } ora_rectangle;
 
+typedef struct ora_layer ora_layer;
+struct ora_layer {
+    ubyte* data;
+    ora_rectangle geometry;
+    int format;
+    float opacity;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -230,13 +238,10 @@ extern int ora_stack_type(ORA ora);
  * If the current element in the stack is a raster layer this function reads its raw data. Only possible if the given document has been opened for reading.
  *
  * @param ora 	the document
- * @param data 	the output parameter that will contain the data of the layer
- * @param geometry 	the output parameter that will contain the geometry of the layer
- * @param format 	the output parameter that will contain the format of the layer
- * @param opacity 	the output parameter that will contain the opacity information of the layer
+ * @param layer the ora_layer to read the data into
  * @param callback 	the optional callback function that can be used to retrieve progress information of the operation. 
  */
-extern int ora_read_layer(ORA ora, ubyte** data, ora_rectangle* geometry, int* format, float* opacity, ora_progress_callback callback);
+extern int ora_read_layer(ORA ora, ora_layer* layer, ora_progress_callback callback);
 
 /** @brief Closes the given ORA document and frees allocated memory.
  *

src/tool/oratool.c
@@ -127,10 +127,7 @@ void tool_progress_callback(int progress)
 int main( int argc, char** argv) { 
     char filename[256];
     ORA ora_in, ora_out;
-    ubyte* image;
-    int format;
-    ora_rectangle geometry;
-    float opacity;
+    ora_layer layer;
     int i;
     long timer, total, read, write;
 
@@ -159,8 +156,8 @@ int main( int argc, char** argv) {
             printf("Reading layer: %d - ", i);
 
             timer = clock();
-            image = NULL;
-            ora_read_layer(ora_in, &image, &geometry, &format, &opacity, tool_progress_callback);
+            layer.data = NULL;
+            ora_read_layer(ora_in, &layer, tool_progress_callback);
 
             timer = clock() - timer;
             timer = timer > 0 ? timer : 0;
@@ -173,15 +170,16 @@ int main( int argc, char** argv) {
             printf("Writing layer: %d - ", i);
 
             timer = clock();
-            ora_write_layer(ora_out, NULL, geometry, format, opacity, image, tool_progress_callback);
+            ora_write_layer(ora_out, NULL, layer.geometry, layer.format, layer.opacity, 
+                layer.data, tool_progress_callback);
 
             timer = clock() - timer;
             timer = timer > 0 ? timer : 0;
             printf("\nDone (%ld ms)\n", (timer / (CLOCKS_PER_SEC / 1000)));
             write += timer;
 
-            if (image)
-                free(image);
+            if (layer.data)
+                free(layer.data);
         } else 
             break;
     }

commit 16b935e6aa46743d8382069497ddda697d8c11c4

Fix regression in reading out layer attributes.
Bug introduced in commit c1e55691fbdd3319d1d205281991de78588468b3

src/lib/ora.c
@@ -669,7 +669,11 @@ extern int ora_read_layer(ORA ora, ora_layer* layer, ora_progress_callback callb
         return ORA_ERROR;
     }
 
+    /* Set output variables */
     layer->visibility = layer_data->visibility;
+    layer->opacity = layer_data->opacity;
+    layer->geometry.x = layer_data->bounds.x;
+    layer->geometry.y = layer_data->bounds.y;
 
     return ORA_OK;
 }